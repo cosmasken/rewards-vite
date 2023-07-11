b042df6efcf45510ca7a9de1adee357a072bf55e
cosmasaruba <15275726+cosmasken@users.noreply.github.com>
2023-07-11 12:17:45 +0300

    shit man

--- src/components/ReferralCard.vue
+++ src/components/ReferralCard.vue
@@ -8,7 +8,7 @@ export default {
 <template>
     <div class=" w-full h-full">
 <div class="flex flex-row rounded-full justify-between gap-4 bg-[#7B22D4]">
-    <img src=".,/assets/icon.svg" class="hidden sm:block h-[64px] w-[64px]" >
+    <img src="../assets/icon.svg" class="hidden sm:block h-[64px] w-[64px]" >
     <p class="font-normal text-sm text-white">
 {{ number }}
 </p>
