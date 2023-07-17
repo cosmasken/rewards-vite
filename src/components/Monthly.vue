45c4fc54dfd5c0e9b71473cca7d3ae33594dfa0f
cosmasken <15275726+cosmasken@users.noreply.github.com>
2023-07-17 16:55:22 +0300

    centered monthly

--- src/components/Monthly.vue
+++ src/components/Monthly.vue
@@ -1,17 +1,17 @@
 <template>
 
 
-<div className=" flex flex-col gap-6 w-full h-40 items-center pr-[331px] py-2 ">
-  <div className="text-center whitespace-nowrap text-4xl font-semibold  leading-10 text-white w-full">
+<div className=" flex flex-col gap-6 w-full h-40 items-center  py-2 text-center">
+  <p className="text-center whitespace-nowrap text-4xl font-semibold  leading-10 text-white">
     Top 10 Campus Ambassador Performers
-  </div>
-  <div className="bg-black/12 self-end flex flex-row mr-32 gap-20 items-center pl-2 pr-20 py-1 rounded-[40px]">
+  </p>
+  <div className="bg-black/20  flex flex-row mr-32 gap-20 items-center pl-2 pr-20 py-1 rounded-[40px]">
     <div className="bg-white flex flex-col w-[211px] shrink-0 h-8 items-center py-1 rounded-[40px]">
-      <div className="font-semibold  text-textblue w-16">
+      <div className="font-semibold text-base  text-textblue ">
         Monthly
       </div>
     </div>
-    <div className="font-semibold  text-white/70 w-16 shrink-0">
+    <div className="font-semibold text-base text-white/70  shrink-0">
       Quarterly
     </div>
   </div>
